ec70f4f4cf0cdf0aab4f98935f1c2e97fec4b772
sayak raha <sayakraha12@gmail.com>
2024-06-21 20:26:07 +0530

    linking html templaces - django.shortcuts.render

--- blogler/blogler/views.py
+++ blogler/blogler/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 def home(req):
-    return HttpResponse("This is the home page")
+    # return HttpResponse("This is the home page")
+    return render(req, 'home.html')
 def about(req):
-    return HttpResponse("this is the about page")
+    # return HttpResponse("this is the about page")
+    return render(req, 'about.html')
